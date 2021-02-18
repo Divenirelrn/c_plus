7c70b73e77f45091614a9e3b192fc18bf8b6d0f6
zhaolu <zhaolu16120284@163.com>
2021-02-18 10:29:36 +0800

    添加introduction

---
+++ introduction/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * 多行注释
+ *
+ * #define PI 3.1415926
+ * const int a = 10;
+ *
+ * 关键字：
+ *   auto
+ *   break
+ *   new
+ *   try
+ *   this
+ *
+ * 标识符：
+ *   区分大小写
+ *
+ */
+
+int main() {
+    const int a = 10; //变量,const 变量，只读属性
+    cout << "a=" << a <<endl;
+    system("pause"); //单行注释
+    cout << "Hello World!" << endl;
+    return 0;
+}
