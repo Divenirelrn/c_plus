28787bb9639b4e69ee94a5106af54fd1d8bbcea8
zhaolu <zhaolu16120284@163.com>
2021-02-18 11:53:50 +0800

    添加流程控制语句

---
+++ procedure_control/cmake-build-debug/CMakeFiles/procedure_control.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/procedure_control.dir/main.cpp.obj"
+  "libprocedure_control.dll.a"
+  "procedure_control.exe"
+  "procedure_control.exe.manifest"
+  "procedure_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/procedure_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ procedure_control/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+/*
+ * 流程控制语句
+ *    if(...)
+ *    {
+ *       ...
+ *    }
+ *    else if(...)
+ *    {
+ *       ...
+ *    }
+ *    else
+ *    {
+ *       ...
+ *    }
+ *
+ *   三目运算符：
+ *     c = a>b ? a : b;
+ *
+ *   switch语句
+ *
+ * 循环结构：
+ *    while(...)
+ *    {
+ *       ...
+ *    }
+ *
+ *    do
+ *    {
+ *     ...
+ *    }while(...);
+ *
+ *    for(...)
+ *    {
+ *      ...
+ *    }
+ *
+ *    跳转语句：
+ *       break
+ *       continue
+ *       goto
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
