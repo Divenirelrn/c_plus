5874f20d862eb9bf5a13696c4ea01831f0de2b39
zhaolu <zhaolu16120284@163.com>
2021-02-21 12:27:21 +0800

    添加stack容器

---
+++ container/container/stack.cpp
@@ -0,0 +1,33 @@
+/*
+* stack容器
+* 
+*	先进后出
+* 
+*	无法遍历
+* 
+*	empty()
+*	size()
+*	push()
+*	pop()
+* 
+* 常用接口：
+*	#include<stack>
+* 
+*	stack<int>s;
+*	s.push(10);
+*	s.push(20);
+*	s.push(30);
+*	s.push(40);
+* 
+*	while(!s.empty())
+*	{
+*		cout << s.top() << endl;
+*		s.pop();
+*	}
+* 
+*	cout << s.size() << endl;
+* 
+* 
+* 
+* 
+*/
