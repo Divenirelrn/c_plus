37bee1c1a0a6c1e06d418cb436f0143ec3fd4982
zhaolu <zhaolu16120284@163.com>
2021-02-21 16:32:04 +0800

    添加算术生成算法

---
+++ algorithms/algorithms/arithmetic_generation.cpp
@@ -0,0 +1,37 @@
+/*
+* 常用算术生成算法
+* 
+* accumulate
+*	计算区间内元素的总和（sum）	
+* 
+*	#include<numeric>
+* 
+*	vector<int>v;
+*	v.push_back(10);
+*	v.push_back(20);
+*	v.push_back(30);
+*	v.push_back(40);
+* 
+*	int total = accumulate(v.begin(), v.end(), 0); //0为起始值
+* 
+* fill:
+*	向容器内填充数据
+* 
+*	#include<numeric>
+* 
+*	vector<int>v;
+*	v.resize(10);
+* 
+*	fill(v.begin(), v.end(), 100); //填充
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+*/
